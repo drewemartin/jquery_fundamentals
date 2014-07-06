60f734c97bafd17dc8167056c921b924d0444895
Drew Martin <drew.e.martin@gmail.com>
2014-07-06 14:22:09 -0400

    Working on jquery_exercises

---
+++ jquery_exercises.js
@@ -0,0 +1,13 @@
+// EXERCISE: Change Background
+// $(document).ready(function(){
+// 	$('#target').css("background-color", "purple");
+// });
+
+// EXERCISE: Change Parent
+// $(document).ready(function(){
+// 	var span = $('span').eq(0);
+// 	span.text("I've changed");
+// 	$('body').eq(0).append(span);
+// });
+
+// EXERCISE: Create Clone
